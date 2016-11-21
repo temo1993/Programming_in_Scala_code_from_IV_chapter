62800c480a697a959e01794175a90a858e28f516
Temo <temo.kiknavelidze@gmail.com>
2016-11-21 17:28:41 +0400

    ..object..trait..

--- src/main/scala/codes/chapter4/Summer.scala
+++ src/main/scala/codes/chapter4/Summer.scala
@@ -2,7 +2,7 @@ package codes.chapter4
 
 // In file Summer.scala
 import ChecksumAccumulator.calculate
-object Summer {
+object Summer{
   def main(args: Array[String]) = {
     for (arg <- args)
       println(arg + ": " + calculate(arg))
